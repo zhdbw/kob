b7ea4c32e45b08e3429f0f4aaef21fec29aac9eb
博雯 张 <1065346495@qq.com>
2023-02-14 18:41:00 +0800

    项目骨架搭建好了

--- backend/src/main/java/com/kob/backend/controller/pk/BotInfoController.java
+++ backend/src/main/java/com/kob/backend/controller/pk/BotInfoController.java
@@ -13,16 +13,15 @@ import java.util.Map;
 public class BotInfoController {
 
     @RequestMapping("getbotinfo")
-    public List<Map<String,String>> getBotInfo() {
-        List<Map<String,String>> list = new LinkedList<>();
+    public Map<String,String> getBotInfo() {
         Map<String,String> bot1 = new HashMap<>();
         bot1.put("name","tiger");
         bot1.put("rating","1500");
-        Map<String,String> bot2 = new HashMap<>();
-        bot2.put("name","cat");
-        bot2.put("rating","100");
-        list.add(bot1);
-        list.add(bot2);
-        return list;
+//        Map<String,String> bot2 = new HashMap<>();
+//        bot2.put("name","cat");
+//        bot2.put("rating","100");
+//        list.add(bot1);
+//        list.add(bot2);
+        return bot1;
     }
 }
